a6a553c6809cd35185f29ad8addfa46aeea80de5
Aditya Kumar Iyer <aditya.kumar.iyer@gmail.com>
2023-05-08 13:41:21 +0530

    fixing an issue

--- src/controllers/inventory.js
+++ src/controllers/inventory.js
@@ -5,13 +5,11 @@ import Item from "../model/item"
 // middleware that is specific to this router
 router.use(express.json())
 
-// define the home page route
 router.get('/list', async (req, res) => {
     let items = await Item.find()
     return res.status(200).json(items)
 })
 
-// define the about route
 router.post('/add', async (req, res) => {
     let item = await Item.findOne({sku: req.body.sku})
     if (item == null) {
